aa918aea25a766260695a2303c1eef9f3b17a0ca
Marawanelhaggar1 <marawanmagdy91@gmail.com>
2023-06-09 05:02:18 +0300

    shop-1

--- online-shop/js/shop.js
+++ online-shop/js/shop.js
@@ -1,15 +1,19 @@
-fetch('http://localhost:5000/api/products')
-  .then(res => res.json())
-  .then(data => {
+fetch("http://localhost:5000/api/products")
+  .then((res) => res.json())
+  .then((body) => body.data)
+  .then((data) => {
     console.log(data);
-    let productHTML = '';
+
+    let productHTML = "";
     for (let i = 0; i < data.length; i++) {
       const product = data[i];
-      productHTML = `
+      productHTML += `
         <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
           <div class="product-item bg-light mb-4">
             <div class="product-img position-relative overflow-hidden">
-              <img class="img-fluid w-100" src="img/product-1.jpg" alt="${product.name}">
+              <img class="img-fluid w-100" src="${product.image}" alt="${
+        product.name
+      }">
               <div class="product-action">
                 <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
                 <a class="btn btn-outline-dark btn-square" href=""><i class="far fa-heart"></i></a>
@@ -18,9 +22,13 @@ fetch('http://localhost:5000/api/products')
               </div>
             </div>
             <div class="text-center py-4">
-              <a class="h6 text-decoration-none text-truncate" href="">${product.name}</a>
+              <a class="h6 text-decoration-none text-truncate" href="">${
+                product.name
+              }</a>
               <div class="d-flex align-items-center justify-content-center mt-2">
-                <h5>$${product.price}</h5><h6 class="text-muted ml-2"><del>${product.price +product.price*0.1}</del></h6>
+                <h5>$${product.price}</h5><h6 class="text-muted ml-2"><del>${
+        product.price + product.price * 0.1
+      }</del></h6>
               </div>
               <div class="d-flex align-items-center justify-content-center mb-1">
                 <small class="fa fa-star text-primary mr-1"></small>
@@ -36,5 +44,5 @@ fetch('http://localhost:5000/api/products')
       `;
     }
 
-    document.getElementById("product").innerHTML = productHTML;
+    document.getElementById("products").innerHTML = productHTML;
   });
